f7663624031dbf2c017f07ff26719bc6b51a7f3a
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-10-04 19:51:46 +0300

    adds index.s

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const remove = document.querySelector("main")
+remove.remove("main")
+
+const newHeader = document.createElement('h1')
+newHeader.id='victory'
+newHeader.textContent= "David Parsley is the champion"
+
+document.body.append(newHeader)
